6019341fc83f01f68fd5225cd033fc3fc3d3357a
Valery <i@vvaganov.ru>
2024-07-09 22:14:58 +0300

    RA-04 replaced the java method with the kotlin method using lambda commit{}

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.example.recipeapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.example.recipeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val fragmentManager = supportFragmentManager
 
     private var _binding: ActivityMainBinding? = null
     private val binding
@@ -22,8 +17,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        fragmentManager.beginTransaction()
-            .add(R.id.mainContainer, CategoriesListFragment())
-            .commit()
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<CategoriesListFragment>(R.id.mainContainer)
+            }
+        }
     }
 }
