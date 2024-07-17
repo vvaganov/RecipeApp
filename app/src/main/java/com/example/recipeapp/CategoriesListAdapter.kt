8d52d2441d6bc30b919598b8f43c4f1c7a8f5234
Valery <i@vvaganov.ru>
2024-07-17 17:05:35 +0300

    RA-10 implemented the transfer of parameters to a new fragment by click

--- app/src/main/java/com/example/recipeapp/CategoriesListAdapter.kt
+++ app/src/main/java/com/example/recipeapp/CategoriesListAdapter.kt
@@ -14,7 +14,7 @@ class CategoriesListAdapter(private val dataSet: List<Category>) :
     RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
 
     interface OnItemClickListener {
-        fun onItemClick()
+        fun onItemClick(itemId: Int)
     }
 
     private var itemClickListener: OnItemClickListener? = null
@@ -37,17 +37,19 @@ class CategoriesListAdapter(private val dataSet: List<Category>) :
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val category = dataSet[position]
-        try {
-            val inputStream: InputStream? =
-                viewHolder.itemView.context?.assets?.open(category.imageUrl)
-            val drawable = Drawable.createFromStream(inputStream, null)
-            viewHolder.imageViewTitle.setImageDrawable(drawable)
-        } catch (e: Exception) {
-            Log.e("!!!", e.stackTrace.toString())
+        with(viewHolder) {
+            try {
+                val inputStream: InputStream? =
+                    itemView.context?.assets?.open(category.imageUrl)
+                val drawable = Drawable.createFromStream(inputStream, null)
+                viewHolder.imageViewTitle.setImageDrawable(drawable)
+            } catch (e: Exception) {
+                Log.e("!!!", e.stackTrace.toString())
+            }
+            textViewTitle.text = category.title
+            textViewDescription.text = category.description
+            itemView.setOnClickListener { itemClickListener?.onItemClick(category.id) }
         }
-        viewHolder.textViewTitle.text = category.title
-        viewHolder.textViewDescription.text = category.description
-        viewHolder.itemView.setOnClickListener { itemClickListener?.onItemClick() }
     }
 
     override fun getItemCount() = dataSet.size
--- app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
+++ app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
@@ -1,10 +1,12 @@
 package com.example.recipeapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.example.recipeapp.databinding.FragmentListCategoriesBinding
@@ -34,19 +36,28 @@ class CategoriesListFragment : Fragment() {
     private fun initRecycler() {
         val customAdapter = CategoriesListAdapter(STUB.getCategories())
         categoriesListBinding.rvCategories.adapter = customAdapter
-        openRecipesByCategoryId(customAdapter)
-    }
-
-    private fun openRecipesByCategoryId(adapter: CategoriesListAdapter) {
-        adapter.setOnItemClickListener(
+        customAdapter.setOnItemClickListener(
             object : CategoriesListAdapter.OnItemClickListener {
-                override fun onItemClick() {
-                    parentFragmentManager?.commit {
-                        setReorderingAllowed(true)
-                        replace<RecipesListFragment>(R.id.mainContainer)
-                    }
+                override fun onItemClick(itemId: Int) {
+                    openRecipesByCategoryId(itemId)
                 }
             }
         )
+    }
+
+    private fun openRecipesByCategoryId(itemId: Int) {
+        val category = STUB.getCategories()
+        val categoryName: String = category.first { it.id == itemId }.title
+        val categoryImageUrl: String = category.first { it.id == itemId }.imageUrl
+
+        val bundle = bundleOf(
+            "ARG_CATEGORY_ID" to itemId,
+            "ARG_CATEGORY_NAME" to categoryName,
+            "ARG_CATEGORY_IMAGE_URL" to categoryImageUrl,
+        )
+        parentFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<RecipesListFragment>(R.id.mainContainer, args = bundle)
+        }
     }
 }
