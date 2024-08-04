cbf63d4cabe7a6adc932784b45ec49cb17eee881
Valery <i@vvaganov.ru>
2024-08-04 09:16:11 +0500

    RA-14 calculation of ingredients from the number of servings is implemented

--- app/src/main/java/com/example/recipeapp/IngredientsAdapter.kt
+++ app/src/main/java/com/example/recipeapp/IngredientsAdapter.kt
@@ -9,9 +9,12 @@ import androidx.recyclerview.widget.RecyclerView
 class IngredientsAdapter(private val dataSet: List<Ingredient>?) :
     RecyclerView.Adapter<IngredientsAdapter.ViewHolder>() {
 
+    private var quantity: Int = 1
+
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
         val textIngredientViewTitle: TextView = view.findViewById(R.id.tvIngredientTitle)
-        val textUnitOfMeasureViewTitle: TextView =
+        var textUnitOfMeasureViewTitle: TextView =
             view.findViewById(R.id.tvIngredientUnitOfMeasureTitle)
     }
 
@@ -23,11 +26,24 @@ class IngredientsAdapter(private val dataSet: List<Ingredient>?) :
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val recipe = dataSet?.get(position)
+        val quantityDouble = recipe?.quantity?.toDouble()?.times(quantity)
+        var quantity = ""
+        if (quantityDouble != null) {
+            quantity = if (quantityDouble % 1.0 == 0.0) {
+                quantityDouble.toInt().toString()
+            } else {
+                quantityDouble.toString()
+            }
 
-        with(viewHolder) {
-            textIngredientViewTitle.text = recipe?.description
-            textUnitOfMeasureViewTitle.text = recipe?.quantity + " " + recipe?.unitOfMeasure
+            with(viewHolder) {
+                textIngredientViewTitle.text = recipe?.description
+                textUnitOfMeasureViewTitle.text = quantity + " " + recipe?.unitOfMeasure
+            }
         }
+    }
+
+    fun updateIngredients(process: Int) {
+        quantity = process
     }
 
     override fun getItemCount() = dataSet?.size ?: 0
