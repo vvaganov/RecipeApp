a7dd5217de67a7da9631462be744cafa12fd4079
Valery <i@vvaganov.ru>
2024-09-14 22:38:07 +0500

    RA-23 transferring logic from a fragment to a viewmodel

--- app/src/main/java/com/example/recipeapp/ui/recipes/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/recipeapp/ui/recipes/recipe/RecipeFragment.kt
@@ -1,9 +1,7 @@
 package com.example.recipeapp.ui.recipes.recipe
 
 import android.annotation.SuppressLint
-import android.content.Context.MODE_PRIVATE
 import android.graphics.drawable.Drawable
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -13,15 +11,12 @@ import android.view.ViewGroup
 import android.widget.SeekBar
 import androidx.core.view.setPadding
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
-import com.example.recipeapp.PREF_FAVORITE_KEY
-import com.example.recipeapp.PREF_FILE_NAME
+import com.example.recipeapp.ARG_RECIPE_ID
 import com.example.recipeapp.R
+import com.example.recipeapp.data.STUB
 import com.example.recipeapp.databinding.FragmentRecipeBinding
-import com.example.recipeapp.model.Recipe
 import com.google.android.material.divider.MaterialDividerItemDecoration
 import java.io.InputStream
-import kotlin.collections.MutableSet
 
 class RecipeFragment : Fragment() {
 
@@ -31,7 +26,8 @@ class RecipeFragment : Fragment() {
         FragmentRecipeBinding.inflate(layoutInflater)
     }
 
-    private var recipe: Recipe? = null
+    private var recipeId: Int? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,23 +38,17 @@ class RecipeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initRecipeArguments()
+        initRecipeIdArguments()
         initUI()
         initRecycler()
-        viewModel.recipeState.observe(viewLifecycleOwner) { it ->
-            Log.i("!!!", "observerWork-${it?.isFavorites}")
-        }
     }
 
-    private fun initRecipeArguments() {
-        recipe = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            arguments?.getParcelable("ARG_RECIPE", Recipe::class.java)
-        } else {
-            arguments?.getParcelable("ARG_RECIPE")
-        }
+    private fun initRecipeIdArguments() {
+        recipeId = arguments?.getInt(ARG_RECIPE_ID)
     }
 
     private fun initRecycler() {
+        val recipe = STUB.getRecipeById(recipeId)
         val customAdapterIngredient = IngredientsAdapter(recipe?.ingredients)
         val customAdapterMethod = MethodAdapter(recipe?.method)
         with(recipeBinding) {
@@ -107,52 +97,31 @@ class RecipeFragment : Fragment() {
 
     private fun initUI() {
 
-        with(recipeBinding) {
-            tvRecipeTitle.text = recipe?.title
-            try {
-                val inputStream: InputStream? =
-                    context?.assets?.open("${recipe?.imageUrl}")
-                val drawable = Drawable.createFromStream(inputStream, null)
-                imgRecipe.setImageDrawable(drawable)
-            } catch (e: Exception) {
-                Log.e("!!!", e.stackTrace.toString())
-            }
+        viewModel.loadRecipe(recipeId)
 
-            val isFavorite = getFavorites().toList().contains(recipe?.id.toString())
+        viewModel.recipeState.observe(viewLifecycleOwner) { it ->
+            with(recipeBinding) {
+                tvRecipeTitle.text = it?.recipe?.title
 
-            if (isFavorite)
-                ibFavorites.setImageResource(R.drawable.ic_heart)
-            else
-                ibFavorites.setImageResource(R.drawable.ic_heart_empty)
-
-            ibFavorites.setOnClickListener {
-                if (!getFavorites().toList().contains(recipe?.id.toString())) {
-                    ibFavorites.setImageResource(R.drawable.ic_heart)
-                    val favoriteSet = getFavorites()
-                    val newSet = favoriteSet.plus(recipe?.id.toString())
-                    setFavorites(newSet)
-                } else {
-                    ibFavorites.setImageResource(R.drawable.ic_heart_empty)
-                    val favoriteSet = getFavorites()
-                    val newSet = favoriteSet.minus(recipe?.id.toString())
-                    setFavorites(newSet)
+                try {
+                    val inputStream: InputStream? =
+                        context?.assets?.open("${it?.recipe?.imageUrl}")
+                    val drawable = Drawable.createFromStream(inputStream, null)
+                    imgRecipe.setImageDrawable(drawable)
+                } catch (e: Exception) {
+                    Log.e("!!!", e.stackTrace.toString())
                 }
+
+                if (it?.isFavorites == true)
+                    ibFavorites.setImageResource(R.drawable.ic_heart)
+                else
+                    ibFavorites.setImageResource(R.drawable.ic_heart_empty)
+
+                ibFavorites.setOnClickListener {
+                    viewModel.onFavoritesClicked(recipeId)
+                }
+
             }
         }
-    }
-
-    private fun setFavorites(setFavoriteId: Set<String>?) {
-        val sharedPref =
-            activity?.getSharedPreferences(PREF_FILE_NAME, MODE_PRIVATE)
-                ?: return
-        sharedPref.edit()?.putStringSet(PREF_FAVORITE_KEY, setFavoriteId)?.apply()
-    }
-
-    private fun getFavorites(): MutableSet<String> {
-        val sharedPref =
-            activity?.getSharedPreferences(PREF_FILE_NAME, MODE_PRIVATE)
-        val setId = sharedPref?.getStringSet(PREF_FAVORITE_KEY, emptySet())
-        val mutableSteId = HashSet<String>(setId)
-        return mutableSteId
     }
 }
