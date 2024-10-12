b9e72667d5b8d9e5e7593f02808620879223cd66
Valery <i@vvaganov.ru>
2024-10-12 13:52:37 +0500

    RA-32

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.recipeapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
 import com.example.recipeapp.databinding.ActivityMainBinding
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +24,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val thread = Thread {
+            val url = URL("https://recipes.androidsprint.ru/api/category")
+            val connection = url.openConnection() as HttpURLConnection
+            connection.connect()
+
+            Log.i("!!!", "ResponseCode: ${connection.responseCode} ")
+            Log.i("!!!", "ResponseMessage: ${connection.responseMessage} ")
+            Log.i("!!!", "Body: ${connection.inputStream.bufferedReader().readText()} ")
+        }
+        thread.start()
 
         binding.btnCategory.setOnClickListener {
             findNavController(R.id.my_nav_host_fragment).navigate(
