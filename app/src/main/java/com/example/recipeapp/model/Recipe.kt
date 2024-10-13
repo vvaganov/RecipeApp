d46fe648fcca6c78bad781854fb507c5f4ea38a6
Valery <i@vvaganov.ru>
2024-10-13 16:55:28 +0500

    RA-32 implemented getting a list of recipes by category id, each in a separate thread

--- app/src/main/java/com/example/recipeapp/model/Recipe.kt
+++ app/src/main/java/com/example/recipeapp/model/Recipe.kt
@@ -2,9 +2,11 @@ package com.example.recipeapp.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
 
 
 @Parcelize
+@Serializable
 data class Recipe(
     val id: Int,
     val title: String,
